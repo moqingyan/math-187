cca8a984dcbed83549e4215210fbef1427d4d667
Jiani Huang <cs100sba@ieng6-201.ucsd.edu>
2016-04-10 18:27:40 -0700

    finished debugging

--- MHKCipher.cpp
+++ MHKCipher.cpp
@@ -11,12 +11,12 @@ int find_inverse(int to_inv, int prime);
 int main(){
 	//MHK cipher takes in a super increasing sequence
 	//This is to change
-	vector<int> origin_super = {1,2,3};
-	int large_p = 3;
-	int a = 2;
-
-	get_pb(origin_super, large_p, a);
-	
+	vector<int> origin_super = {3,5,12,21,43};
+	int large_p = 89;
+	int a = 15;
+    int secret_message = 10;
+	//get_pb(origin_super, large_p, a);
+	decrypt_MHK(secret_message, large_p, a, origin_super);
 }
 
 vector<int> get_pb(vector<int> origin_super, int large_p, int a){
@@ -25,33 +25,42 @@ vector<int> get_pb(vector<int> origin_super, int large_p, int a){
 	int to_push = 0;
 	cout << "the public key is: ";
 	for(int ct = 0; ct < origin_super.size(); ct++){
-		to_push = origin_super[ct] * a % large_p);
+		to_push = origin_super[ct] * a % large_p;
 		encrypted_super.push_back( to_push );
 		cout << to_push << ", ";
 	}
 	cout<<endl;
+    return encrypted_super;
 }
 
 vector<int> decrypt_MHK(int secret_message, int large_p, int a, vector<int> origin_super){
 	
 	int inv_a = find_inverse(a, large_p);
-	vector<int> decrypted;
+	
+    cout << "This is inv_a: " << inv_a << endl;
+
+    vector<int> decrypted;
 	int message = inv_a * secret_message % large_p;
 
-	cout << "the message is: ";
-	for(int ct = 0; ct < origin_super.size(); ct++){
+    cout << "This is message: " << message << endl;
+	cout << "the reversed eessage is: ";
+	for(int ct = origin_super.size()-1; ct >= 0; ct--){
 
-		if(message > origin_super[ct]){
-			cout << "1 " << endl;
+		if(message >= origin_super[ct]){
+			cout << "1 ";
 			decrypted.push_back(1);
-		}
+            message = message - origin_super[ct];
+        }
 
-		else if{
-			cout << "0 " << endl;
+		else{
+			cout << "0 ";
 			decrypted.push_back(0);
 		}
+
 	}
 
+
+     cout << endl;
 	return decrypted;
 
 }
@@ -63,4 +72,5 @@ int find_inverse(int to_inv, int prime){
 		}
 	}
 	cout << "chose a bad number" << endl;
+    return -1;
 }
